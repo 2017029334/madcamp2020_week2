9e0989890d31ef4afe775a52a4d6752e23cc2f9c
2017029334 <ghio12@naver.com>
2020-07-18 15:38:11 +0900

    login

---
+++ only_login/youtube/app/src/main/java/com/example/youtube/login/RetrofitInterface.java
@@ -0,0 +1,19 @@
+package com.example.youtube.login;
+
+import com.example.youtube.login.LoginResult;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitInterface {
+
+    @POST("/login")
+    Call<LoginResult> executeLogin(@Body HashMap<String, String> map);
+
+    @POST("/signup")
+    Call<Void> executeSignup (@Body HashMap<String, String> map);
+
+}
